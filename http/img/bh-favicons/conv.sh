af9c803889251bac9d87121cd963e5abd63f30a2
beedaan <beedaan@xubu.(none)>
2014-04-16 21:18:33 -0400

    Create BH favicon

---
+++ http/img/bh-favicons/conv.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+convert favicon-228.png -resize 195x195 favicon-195.png
+convert favicon-228.png -resize 152x152 favicon-152.png
+convert favicon-228.png -resize 144x144 favicon-144.png
+convert favicon-228.png -resize 128x128 favicon-128.png
+convert favicon-228.png -resize 120x120 favicon-120.png
+convert favicon-228.png -resize 114x114 favicon-114.png
+convert favicon-228.png -resize 96x96 favicon-96.png
+convert favicon-228.png -resize 72x72 favicon-72.png
+convert favicon-228.png -resize 64x64 favicon-64.png
+convert favicon-228.png -resize 57x57 favicon-57.png
+convert favicon-228.png -resize 48x48 favicon-48.png
+convert favicon-228.png -resize 32x32 favicon-32.png
+convert favicon-228.png -resize 24x24 favicon-24.png
+convert favicon-228.png -resize 16x16 favicon-16.png
