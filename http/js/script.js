7b8375eec0d55400e69395d8bdc98f23724d9aa5
Brendan Heussler <brendan.heussler@inovexcorp.com>
2014-04-16 23:50:32 -0400

    Animate page with jquery
    The hover area seems to have decreased

--- http/js/script.js
+++ http/js/script.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
-	$("header").fadeIn(2000, function() {
-		$("header").transition({ y:'40px'});
-	});
+$(function() {
+	init();
 });
+
+function init() {
+	$("header").transition({opacity:1}, 800, 'linear');
+	$("body").transition({y:'2em'}, 1400, 'easeOutQuint', function() {
+		unfold("a:first");
+	});
+}
+
+function unfold(node) {
+	$(node).transition({perspective: '400px', rotateX: '-360deg', opacity: 1}, 100, 'snap', function() {
+		unfold(this.next());
+	});
+}
