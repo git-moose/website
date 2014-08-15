420ed5275b5f6a379c4d32fb3417e9da0b082221
Brendan Heussler <bheussler@gmail.com>
2014-08-15 10:05:16 -0400

    move everything out of http

---
+++ img/bh-favicons/fav.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+convert favicon-16.png favicon-24.png favicon-32.png favicon-48.png favicon-64.png favicon.ico
---
+++ js/trans.js
@@ -0,0 +1,11 @@
+$(function() {
+	init();
+});
+
+function init() {
+	$('header').addClass('animated fadeInDown');
+	$('header').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+		$('#content').css('opacity',1);
+		$('#content').addClass('animated bounceInUp');
+	});
+}
