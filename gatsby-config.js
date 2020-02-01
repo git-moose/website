c664fa89432670004d9bc60297b5b7425fa3133a
Brendan Heussler <brendan@lifejourney.us>
2020-01-31 17:30:12 -0800

    Strip out the existing content and replace with my own

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,71 +1,71 @@
 require(`dotenv`).config({
-  path: `.env`,
+    path: `.env`,
 })
 
 module.exports = {
-  siteMetadata: {
-    siteTitle: `Brendan Heussler`,
-    siteTitleAlt: `Brendan Heussler`,
-    siteUrl: `https://builtbybrendan.com`,
-    siteDescription: `The personal website for Brendan Heussler, a Software Engineer living in San Diego, California.  Writes on topics such as Java, Javascript, and anything in between`,
-    siteLanguage: `en`,
-    author: `@beedaan`,
-  },
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-      options: {
-        navigation: [
-          {
-            title: `Blog`,
-            slug: `/blog`,
-          },
-          {
-            title: `About`,
-            slug: `/about`,
-          },
-        ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/beedaan`,
-          },
-        ],
-      },
+    siteMetadata: {
+        siteTitle: `Brendan Heussler`,
+        siteTitleAlt: `Brendan Heussler`,
+        siteUrl: `https://builtbybrendan.com`,
+        siteDescription: `The personal website for Brendan Heussler, a Software Engineer living in San Diego, California.  Writes on topics such as Java, Javascript, and anything in between`,
+        siteLanguage: `en`,
+        author: `@beedaan`,
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#6B46C1`,
-        display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-    // `gatsby-plugin-webpack-bundle-analyser-v2`,
-  ],
+    plugins: [
+        {
+            resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+            options: {
+                navigation: [
+                    {
+                        title: `Blog`,
+                        slug: `/blog`,
+                    },
+                    {
+                        title: `About`,
+                        slug: `/about`,
+                    },
+                ],
+                externalLinks: [
+                    {
+                        name: `Twitter`,
+                        url: `https://twitter.com/beedaan`,
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GOOGLE_ANALYTICS_ID,
+            },
+        },
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Brendan Heussler - Software Engineer is San Diego`,
+                short_name: `Brendan Heussler`,
+                description: `I'm Brendan, currently living in San Diego, California. I'm a software engineer at Swing AI. Currently, I'm working on a side project and writing about it.`,
+                start_url: `/`,
+                background_color: `#fff`,
+                theme_color: `#6B46C1`,
+                display: `standalone`,
+                icons: [
+                    {
+                        src: `/android-chrome-192x192.png`,
+                        sizes: `192x192`,
+                        type: `image/png`,
+                    },
+                    {
+                        src: `/android-chrome-512x512.png`,
+                        sizes: `512x512`,
+                        type: `image/png`,
+                    },
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-netlify`,
+        // `gatsby-plugin-webpack-bundle-analyser-v2`,
+    ],
 }
